6aafd0a2b79adb111cbdd241ae55c4d4f04938df
Egor Orlenok <messaf@mail.ru>
2023-09-01 16:36:22 +1000

    Add project to repo

---
+++ MqttAgent/Form1.cs
@@ -0,0 +1,82 @@
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace MqttAgent
+{
+    public partial class FormSettings : Form
+    {
+
+        public FormSettings()
+        {
+            InitializeComponent();
+            SettingsOperate settingsOperate= new SettingsOperate();
+            Debug.WriteLine("Start MQTT Agent");
+        }
+
+        private void FormSettings_Deactivate(object sender, EventArgs e)
+        {
+
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.ShowInTaskbar = false;
+                agentIconTray.Visible = true;
+            }
+        }
+
+
+        private void FormSettings_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void agentIconTray_MouseClick(object sender, MouseEventArgs e)
+        {
+            //if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            //{
+            //    if (this.WindowState == FormWindowState.Minimized)
+            //    {
+            //        this.WindowState = FormWindowState.Normal;
+            //        this.ShowInTaskbar = true;
+            //        agentIconTray.Visible = false;
+            //    }
+            //}
+
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                contextMenuStrip1.Show(Cursor.Position);
+            }
+        }
+
+        private void FormSettings_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                e.Cancel = true;
+                this.WindowState = FormWindowState.Minimized;
+                this.ShowInTaskbar = false;
+                agentIconTray.Visible = true;
+            }
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void SettingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.WindowState = FormWindowState.Normal;
+                this.ShowInTaskbar = true;
+                agentIconTray.Visible = false;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MqttAgent/SettingsOperate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace MqttAgent
+{
+    internal class SettingsOperate
+    {
+        const string settingsFolderName = "Settings";  // для Windows
+        string settingFolderPath = ""; 
+
+        public SettingsOperate()
+        {
+            settingFolderPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData), settingsFolderName);
+            Debug.WriteLine(settingFolderPath);
+
+        }
+    }
+}
